d1e4f669e7c06d213b90b90a2f739c055e3d0946
Matheus <matheusdeamorim20@gmail.com>
2023-02-17 14:47:37 -0300

    Adicionando Carregando...

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,11 @@
 const api_url = 'https://api.adviceslip.com/advice'
 // fetch('https://api.adviceslip.com/advice').then(response =>)
 
+// Função: mostra o conselho na tela
 async function getAdviceIndex() {
+    
+    // Label de carregando enquanto a função faz a request pra API
+    document.getElementById('advice_results').innerHTML = `Carregando...`
 
     const response = await fetch(api_url);
     const adviceData = await response.json();
@@ -16,6 +20,7 @@ getAdviceIndex();
 
 const btnAdv = document.getElementById('get_advice')
 
+// Evento de clique que executa a função 
 btnAdv.addEventListener('click', () => {
     getAdviceIndex();
 })
